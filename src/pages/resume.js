0f2d2665d84a077dd7d88008fed8a3f35b4c8cdb
AryanB1 <62898176+AryanB1@users.noreply.github.com>
2022-10-03 15:46:18 -0400

    updated resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -6,9 +6,9 @@ function Resume() {
         <div style={{textAlign: "center"}}>
             <h1>Resume</h1>
             <hr />
-            <iframe src="https://drive.google.com/file/d/1Pr438gXf2PTcTPqq27olebejXrk8cmoJ/preview" width="100%" height="100%" title="Resume"></iframe>
+            <iframe src="https://drive.google.com/file/d/1uoFF-ihIhuUFrh1ZvGS7mFlPIgwaSpgx/preview" width="100%" height="100%" title="Resume"></iframe>
             <hr />
-            <a href="https://drive.google.com/file/d/1Pr438gXf2PTcTPqq27olebejXrk8cmoJ/view?usp=sharing" target="_blank" rel="noreferrer"><button>Link</button></a>
+            <a href="https://drive.google.com/file/d/1uoFF-ihIhuUFrh1ZvGS7mFlPIgwaSpgx/view?usp=sharing" target="_blank" rel="noreferrer"><button>Link</button></a>
         </div>
     );
 }
