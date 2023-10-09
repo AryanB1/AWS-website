3881d3004fea2a1027a934873ec42502f620cfd6
AryanB1 <aryan.bhardwaj@uwaterloo.ca>
2023-10-08 18:11:34 -0600

    Updated Resume

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -6,9 +6,9 @@ function Resume() {
         <div style={{textAlign: "center"}}>
             <h1>Resume</h1>
             <hr />
-            <iframe src="https://drive.google.com/file/d/1WTF8UZxuAb0MoPGgCRr9rW_VZ7o1n8_3/preview" width="100%" height="100%" title="Resume"></iframe>
+            <iframe src="https://drive.google.com/file/d/1QTq_w7Nk7b2jwqkZmnAZW4IVSlmpgmWy/preview" width="100%" height="100%" title="Resume"></iframe>
             <hr />
-            <a href="https://drive.google.com/file/d/1WTF8UZxuAb0MoPGgCRr9rW_VZ7o1n8_3/view?usp=sharing" target="_blank" rel="noreferrer"><button>Link</button></a>
+            <a href="https://drive.google.com/file/d/1QTq_w7Nk7b2jwqkZmnAZW4IVSlmpgmWy/view?usp=sharing" target="_blank" rel="noreferrer"><button>Link</button></a>
         </div>
 
     );
